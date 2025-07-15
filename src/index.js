7b82336356d6ad6c857597d0034d631fe1ff7199
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-07-15 16:58:13 +1000

    API call function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import "./styles.css";
 
-console.log("Hello World");
+const URL_BASE = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+const URL_KEY = `?key=${process.env.API_KEY}`
+
+function getData(location){
+  return fetch(URL_BASE+location+URL_KEY)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    return data;
+  })
+  .catch(function(error){
+    console.error(error);
+  });
+}
+
+const melbourne = await getData("Melbourne")
+window.melbourne = melbourne;
